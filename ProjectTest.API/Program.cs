8c0d0f3be2b031d13491809b767f1fa9a060f6d9
wilbert123 <wilbertucs@gmail.com>
2022-12-06 23:11:18 -0500

    add travis

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255";
+    return "POST - Net 6 Running ... 06122022-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
 });
 
 app.Run($"http://0.0.0.0:{port}");
---
+++ scripts/dockerize.sh
@@ -0,0 +1,20 @@
+TAG=''
+VERSION_TAG=
+ 
+case "$TRAVIS_BRANCH" in
+  "main")
+    TAG=latest
+    VERSION_TAG=$TRAVIS_BUILD_NUMBER
+    ;;
+  "develop")
+    TAG=dev
+    VERSION_TAG=$TAG-$TRAVIS_BUILD_NUMBER
+    ;;    
+esac
+ 
+REPOSITORY=$DOCKER_USERNAME/travis-demo-net6
+ 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
+docker push $REPOSITORY:$TAG
+docker push $REPOSITORY:$VERSION_TAG
